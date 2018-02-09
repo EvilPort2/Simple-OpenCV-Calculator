b2d89eb65ac021002c350b1faed3f9c834adc229
EvilPort2 <dibakarsaha1234@gmail.com>
2018-02-10 03:06:52 +0530

    Update

--- hand-calculator.py
+++ hand-calculator.py
@@ -12,24 +12,25 @@ def start_calculator():
 	cam = cv2.VideoCapture(1)
 	numbers = deque(maxlen = 100)
 	x, y, w, h = 300, 100, 300, 300
-	flag_first_number, flag_second_number, flag_clear, flag_new_number = True, False, False, False
+	flag_first_number, flag_operator, flag_second_number, flag_clear, flag_new_number = True, False, False, False, False
 	display_text = ""
+	display_info = ""
 	with open("range.pickle", "rb") as f:
 		t = pickle.load(f)
 	lower = np.array([t[0], t[1], t[2]])					# HSV green lower
 	upper = np.array([t[3], t[4], t[5]])					# HSV green upper
 	x, y, w, h = 300, 100, 300, 300
+
+	display_info = "Enter first number"
+	count_clear_frames = 0
 	while True:
 		_, img = cam.read()
 		img = cv2.flip(img, 1)
 		imgCrop = img[y:y+h, x:x+w]
-		imgGray = cv2.cvtColor(imgCrop, cv2.COLOR_BGR2GRAY)
 		imgHSV = cv2.cvtColor(imgCrop, cv2.COLOR_BGR2HSV)
 		blur = cv2.medianBlur(imgHSV, 15)
 		blur = cv2.GaussianBlur(blur , (5,5), 0)
 		mask = cv2.inRange(blur, lower, upper)
-		masked = cv2.bitwise_and(imgGray, imgGray, mask = mask)
-		#thresh = cv2.adaptiveThreshold(masked,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 		thresh = cv2.threshold(mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
 		contours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
 
@@ -40,7 +41,6 @@ def start_calculator():
 			M = cv2.moments(contour)
 			if M['m00'] != 0:
 				center = np.array([int(M['m10']/M['m00']), int(M['m01']/M['m00'])])
-				center[1] = center[1] + 50
 				cv2.circle(imgCrop, tuple(center), 4, (0, 0, 255), -1)
 			hulls = cv2.convexHull(contour, returnPoints = False)
 			defects = cv2.convexityDefects(contour, hulls)
@@ -68,9 +68,7 @@ def start_calculator():
 
 					list_angleA.append(angleA)
 
-					#print("a =", a, "b =", b, "c =", c, "angleA =", angleA, "angleB =", angleB, "angleC =", angleC)
 					if angleA <= 90:
-						#print("first")
 						fingers += 1
 						cv2.circle(imgCrop, tuple(start), 3, (0,0,255), -1)
 						cv2.circle(imgCrop, tuple(far), 3, (255,255,0), -1)
@@ -84,42 +82,72 @@ def start_calculator():
 						and list_b.index(max(list_b)) == list_c.index(max(list_c))) \
 						and list_angleA[list_a.index(max(list_a))] > 110 \
 						and center_y > end_y:
-					#print("second")
 					fingers = 0
 					cv2.circle(imgCrop, tuple(list_start[list_a.index(max(list_a))]), 3, (0,0,255), -1)
 					cv2.circle(imgCrop, tuple(list_far[list_a.index(max(list_a))]), 3, (255,255,0), -1)
 					cv2.line(imgCrop, tuple(center), tuple(list_end[list_a.index(max(list_a))]), (0,255,0), 3)
 					flag_new_number = True
 				elif np.all(np.array(list_angleA) > 120):
-					#print("third")
 					if flag_new_number:
 						flag_new_number = False
 						if flag_first_number and len(numbers) > 10:
 							flag_first_number = False
 							first = Counter(numbers).most_common(1)[0][0]
 							display_text = str(first)
+							display_info = "Enter operator"
+							flag_operator = True
+						
+						elif flag_operator and len(numbers) > 10:
+							flag_operator = False
+							operator = Counter(numbers).most_common(1)[0][0]
+							if operator == 1:
+								operator = " + "
+							elif operator == 2:
+								operator = " - "
+							elif operator == 3:
+								operator = " * "
+							elif operator == 4:
+								operator = " / "
+							elif operator == 5:
+								operator = " % "
+							display_text = display_text + operator
+							display_info = "Enter second number"
 							flag_second_number = True
+						
 						elif flag_second_number and len(numbers) > 10:
 							flag_second_number = False
 							second = Counter(numbers).most_common(1)[0][0]
-							display_text = str(first)+" + " + str(second)+" = "+str(first+second) 
+							display_text = display_text + str(second)
+							display_text = display_text +" = " + str(eval(display_text))
+							display_info = "Clear screen"
 							flag_clear = True
+
 						elif flag_clear:
+							count_clear_frames = 0
 							flag_clear = False
 							display_text = ""
+							display_info = "Enter first number"
 							flag_first_number = True
+						
+					elif flag_clear and count_clear_frames > 25:
+						count_clear_frames = 0
+						flag_clear = False
+						display_text = ""
+						display_info = "Enter first number"
+						flag_first_number = True
 
 					numbers = deque(maxlen = 100)
 																		
 			fingers += 1
 			numbers.append(fingers)
-			#print(fingers, "\n")
 
-		cv2.putText(img, display_text, (30, 60), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255))
+		if display_info == "Clear screen":
+			count_clear_frames += 1
+		cv2.putText(img, display_text, (30, 60), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 255, 255))
+		cv2.putText(img, display_info, (30, 440), cv2.FONT_HERSHEY_TRIPLEX, 1.0, (255, 255, 255) )
 		cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
 		cv2.imshow("Calculator", img)
 		cv2.imshow("thresh", thresh)
-		#cv2.imshow("masked", masked)
 		if cv2.waitKey(1) == ord('q'):
 			break
 
